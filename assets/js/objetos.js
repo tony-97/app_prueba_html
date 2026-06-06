8bd099e330ba1926f26c604cab513c427f694d5d
tony-97 <taaffaat_1@hotmail.com>
2026-06-05 23:54:52 -0500

    Agregando objetos en javascript

---
+++ assets/js/objetos.js
@@ -0,0 +1,10 @@
+const personas = {
+  nombre: "Juan",
+  apellidos: "Perez",
+  edad: 30,
+};
+
+console.log(personas.nombre);
+console.log(personas.apellidos);
+console.log(personas.edad);
+console.log(personas);
